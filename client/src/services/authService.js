3b09fae59bfed243f63495cf34ddc4da6ef7f6d2
ronishastha495 <shrestharonisha374@gmail.com>
2025-05-04 21:01:12 +0545

    auth completed

---
+++ client/src/services/authService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5259/api/auth"; // adjust if port differs
+
+export const register = async (userData) => {
+  return axios.post(`${API_URL}/register`, userData);
+};
+
+export const login = async (userData) => {
+  return axios.post(`${API_URL}/login`, userData);
+};
